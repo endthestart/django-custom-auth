60f25c0c4bae9b4da30ae0e3974602f41b383409
Michael Anderson <andermic@gmail.com>
2014-01-05 14:44:23 -0600

    Adjust redirects

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth import logout as logout_user
+from django.contrib.auth import logout as logout_user, login as login_user, authenticate
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 
@@ -8,6 +8,8 @@ from .models import User
 
 
 def register(request, template_name='custom_auth/register.html'):
+    if request.user.is_authenticated():
+        return redirect('/')
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -17,10 +19,16 @@ def register(request, template_name='custom_auth/register.html'):
             last_name = request.POST.get('last_name', None)
             password = request.POST.get('password', None)
             user = User.objects.create_user(email, first_name, last_name, password)
-            return redirect('registration_successful')
+            user.save()
+            user = authenticate(username=email, password=password)
+            login_user(request, user)
+            messages.success(request, "You have successfully registered and are now logged in.")
+            return redirect(request.POST.get('next', '/'))
     else:
-        form = RegistrationForm()
-    return render_to_response(template_name, {'form': form,}, RequestContext(request))
+        initial = {'email': request.GET.get('email', None)}
+        form = RegistrationForm(initial=initial)
+    context = {'form': form, 'next': request.GET.get('next', None),}
+    return render_to_response(template_name, context, RequestContext(request))
 
 
 def registration_successful(request, template_name='auser/registration_successful.html'):
@@ -33,13 +41,15 @@ def activate(request, template_name='custom_auth/activate.html'):
 
 
 def login(request, template_name='custom_auth/login.html'):
+    if request.user.is_authenticated():
+        return redirect('/')
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.login(request):
             messages.success(request, "You have successfully logged in.")
-            if hasattr(request.POST, 'redirect_to'):
-                return redirect(request.POST.get('redirect_to'))
-            return redirect('/')
+            return redirect(request.POST.get('next', '/'))
+        else:
+            messages.error(request, "Your username and password did not match.")
     else:
         form = LoginForm()
     return render_to_response(template_name, {'form': form,}, RequestContext(request))
@@ -47,7 +57,9 @@ def login(request, template_name='custom_auth/login.html'):
 
 def logout(request):
     logout_user(request)
+    messages.success(request, "You have successfully logged out.")
     return redirect('auth_login')
+
 
 def manage(request, template_name='custom_auth/manage.html'):
     return render_to_response(template_name, {}, RequestContext(request))
